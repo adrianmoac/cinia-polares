bc4938725e2c98227a32934bf4a64d7048128f20
Adrián Moras <adrianma@Adrians-MacBook-Pro.local>
2025-03-02 16:36:31 -0600

    busqueda por nombre y fecha

--- src/pages/Home/homeView.tsx
+++ src/pages/Home/homeView.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Box, Button, FormControl, IconButton, InputLabel, MenuItem, Paper, Select, TextField, Tooltip } from '@mui/material'
+import { Box, Button, IconButton, Paper, TextField, Tooltip } from '@mui/material'
 import { alpha, styled } from '@mui/material/styles';
 import { DataGrid, GridColDef, gridClasses } from '@mui/x-data-grid';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import EditIcon from '@mui/icons-material/Edit';
 import { useNavigate } from 'react-router-dom';
+import Datepicker from '../../helpers/datepicker';
+import dayjs, { Dayjs } from 'dayjs';
 
 interface Props {
   data: any;
@@ -12,7 +14,9 @@ interface Props {
   totalDocumentsNum: number;
   rowsPerPage: number;
   searchName: string;
-  setSearchName: any;
+  searchDate: Dayjs | Date | null;
+  setSearchName: (e: any) => void;
+  setSearchDate: (e: any) => void;
   handlePageChange: (page: number) => {};
   handleSearch: (name: string) => {};
   handleCleanSearch: () => void;
@@ -55,7 +59,7 @@ const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
 
 
 
-const HomeView: React.FC<Props> = ({ loading, data: rowData, totalDocumentsNum, rowsPerPage, searchName, setSearchName, handlePageChange, handleSearch, handleCleanSearch }) => {
+const HomeView: React.FC<Props> = ({ loading, data: rowData, totalDocumentsNum, rowsPerPage, searchName, searchDate, setSearchName, setSearchDate, handlePageChange, handleSearch, handleCleanSearch }) => {
   const navigate = useNavigate();
 
   const columns: GridColDef[] = [
@@ -106,31 +110,21 @@ const HomeView: React.FC<Props> = ({ loading, data: rowData, totalDocumentsNum, 
     <Box sx={{ marginX: 5, marginTop: 5 }}>
       <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginBottom: 5 }}>
         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2, mb: 3 }}>
-          <Button variant='contained' sx={{ textTransform: 'none', width: 250, marginRight: 0, marginLeft: 'auto', marginBottom: 3 }} onClick={() => window.location.href = 'AgregarAdministrador'}>Agregar administrador </Button>
-          <Button variant='contained' sx={{ textTransform: 'none', width: 250, marginRight: 0, marginLeft: 'auto', marginBottom: 3 }} onClick={() => window.location.href = 'AgregarColaborador'}>Agregar empleado</Button>
-        </Box>
-          <Box display={'flex'} flexDirection={'row'} gap={5}>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 6, mb: 3 }}>
+            <Button variant='contained' sx={{ textTransform: 'none', width: 250, marginRight: 0, marginLeft: 'auto', marginBottom: 3 }} onClick={() => window.location.href = 'AgregarAdministrador'}>Agregar administrador </Button>
+            <Button variant='contained' sx={{ textTransform: 'none', width: 250, marginRight: 0, marginBottom: 3 }} onClick={() => window.location.href = 'AgregarColaborador'}>Agregar empleado</Button>
+          </Box>
+          <Box display={'flex'} flexDirection={'row'} gap={5} justifyContent={'flex-end'}>
             <Box display={'flex'} width={'60%'}>
               <TextField size='small' placeholder='Buscar por nombre' value={searchName} onChange={(e) => setSearchName(e.target.value)} sx={{ width: 450 }}></TextField>
               <Button variant='outlined' onClick={() => handleSearch(searchName)} sx={{ paddingX: 5}}>Buscar</Button>
               <Button variant='outlined' onClick={handleCleanSearch} sx={{ paddingX: 5, color: 'gray', borderColor: 'gray' }}>Limpiar</Button>
             </Box>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label" size={'small'}>Ingresa fecha</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
+              <Datepicker
                 size='small'
-                value={10}
-                label="Ingresa fecha"
-                // onChange={handleChange}
-              >
-                <MenuItem value={10}>Hoy</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
+                value={searchDate}
+                onChange={(e) => setSearchDate(dayjs(new Date(e)))}
+              ></Datepicker>
           </Box>
         </Box>
       </Box>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 import HomeView from './homeView';
-import { collection, getDocs, orderBy, query, startAfter, limit, where, getCountFromServer, setDoc } from 'firebase/firestore';
+import { collection, getDocs, orderBy, query, startAfter, limit, where, getCountFromServer } from 'firebase/firestore';
 import { fs } from '../../firebase';
+import dayjs, { Dayjs } from 'dayjs';
 
 type Props = {}
 
 const HomeWrapper: React.FC<Props> = () => {
   const [data, setData] = useState<any[]>([]);
   const [lastDoc, setLastDoc] = useState<any>(null);
-  const [firstDoc, setFirstDoc] = useState<any>(null);
   const [page, setPage] = useState(0);
   const [searchName, setSearchName] = useState<string>('');
-  const [searchDate, setSearchDate] = useState<string | null>(null);
+  const [searchDate, setSearchDate] = useState<Dayjs | Date>(dayjs(new Date()));
   const [docsInCache, setDocsInCache] = useState<any[]>([]);
   const [totalDocumentsNum, setTotalDocumentsNum] = useState<number>(0);
   const [totalDocumentsNumWillNotChange, setTotalDocumentsNumWillNotChange] = useState<number>(0);
@@ -20,11 +20,11 @@ const HomeWrapper: React.FC<Props> = () => {
   const rowsPerPage = 3;
 
   // Función para cargar datos paginados y rendimiento
-  const fetchData = async (pageChange: number) => {
+  const fetchData = async (pageChange: number, dateChange?: boolean) => {
     setIsLoading(true);
 
     // Verificar si los datos ya están en el caché
-    if (docsInCache[pageChange]) {
+    if (!dateChange && docsInCache[pageChange]) {
       setData(docsInCache[pageChange]);
       setIsLoading(false);
       return;
@@ -35,7 +35,7 @@ const HomeWrapper: React.FC<Props> = () => {
 
     let qWorkers;
 
-    if (lastDoc) {
+    if (!dateChange && lastDoc) {
       qWorkers = query(workersRef, orderBy("nombre"), startAfter(lastDoc), limit(rowsPerPage));
     } else {
       qWorkers = query(workersRef, orderBy("nombre"), limit(rowsPerPage));
@@ -51,7 +51,7 @@ const HomeWrapper: React.FC<Props> = () => {
         const registrosRef = collection(fs, "workers", doc.id, "rendimiento");
         const q = query(
           registrosRef,
-          where("fecha", "==", new Date().toISOString().split('T')[0])
+          where("fecha", "==", searchDate.toISOString().split('T')[0])
         );
         
         const snapshot = await getDocs(q);
@@ -67,7 +67,10 @@ const HomeWrapper: React.FC<Props> = () => {
       }
 
       // Almacenamos los datos de la página en caché
-      const updatedCache = [...docsInCache];
+      let updatedCache: any[] = [];
+      if(!dateChange) {
+        updatedCache = [...docsInCache];
+      }
       updatedCache[pageChange] = workersData;
       setDocsInCache(updatedCache);
 
@@ -80,7 +83,6 @@ const HomeWrapper: React.FC<Props> = () => {
 
       // Actualizamos los datos de la página actual
       setData(workersData);
-      setFirstDoc(workersSnapshot.docs[0]);
       setLastDoc(workersSnapshot.docs[workersSnapshot.docs.length - 1]);
 
     } catch (error) {
@@ -107,7 +109,7 @@ const HomeWrapper: React.FC<Props> = () => {
           const registrosRef = collection(fs, "workers", doc.id, "rendimiento");
           const q = query(
             registrosRef,
-            where("fecha", "==", new Date().toISOString().split('T')[0])
+            where("fecha", "==", new Date(String(searchDate)).toISOString().split('T')[0])
           );
   
           const rendimientoSnapshot = await getDocs(q);
@@ -142,6 +144,16 @@ const HomeWrapper: React.FC<Props> = () => {
   }
 
   useEffect(() => {
+    // Ensure fetchData is called again when the searchDate is updated
+    const fetchDataOnDateChange = async () => {
+      setPage(0);  // Reset page to 0 when the date changes
+      await fetchData(0, true);  // Fetch new data based on the updated searchDate
+    };
+  
+    fetchDataOnDateChange();
+  }, [searchDate]);
+
+  useEffect(() => {
     const fetchTotalDocsNumber = async () => {
       const workersRef = collection(fs, "workers");
       const countSnapshot = await getCountFromServer(workersRef);
@@ -160,7 +172,9 @@ const HomeWrapper: React.FC<Props> = () => {
       totalDocumentsNum={totalDocumentsNum}
       rowsPerPage={rowsPerPage}
       searchName={searchName}
+      searchDate={searchDate}
       setSearchName={setSearchName}
+      setSearchDate={setSearchDate}
       handlePageChange={fetchData}
       handleSearch={handleSearch}
       handleCleanSearch={handleCleanSearch}
